121a842084b3158088fc51215fc5ff22e564de16
kchodkiewicz <you@example.com>
2020-05-06 18:22:53 +0200

    new

--- Operation_O.m
+++ Operation_O.m
@@ -2,10 +2,10 @@ function [E] = Operation_O(c,d)
 
 x = ones(100, 1);
 
-CE = x*c
+CE = x*c; % cylindryczne rozszerzenie
 
-F = min(CE, d);
+F = min(CE, d); % przekroj metoda sup-M
 
-E = max(F,[],1);
+E = max(F,[],1); % projekcja
 end
 
--- main.m
+++ main.m
@@ -20,16 +20,30 @@ AprimTrap = trapmf(X, [2 2.4 2.7 3]);
 
 R1 = Implication_reich(A1, B1);
 R2 = Implication_reich(A2, B2);
-
+% Wnioskowanie
+% dla trapezu
 B1primTrap = Operation_O(AprimTrap, R1);
 B2primTrap = Operation_O(AprimTrap, R2);
-
+% dla trojkata
 B1primTrim = Operation_O(AprimTrim, R1);
 B2primTrim = Operation_O(AprimTrim, R2);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% figure("Name", "trojkąt")
+% hold on;
+% plot(B1primTrim, "-r");
+% plot(B2primTrim, "-b");
+% hold off;
+% figure("Name", "trapez")
+% hold on;
+% plot(B1primTrap, "-r");
+% plot(B2primTrap, "-b");
+% hold off;
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Agregacja
 BtrapMIN = min(B1primTrap, B2primTrap);
 BtrimMIN = min(B1primTrim, B2primTrim);
-
 
  BtrapAVG = zeros(1, 100);
  BtrimAVG = zeros(1, 100);
@@ -40,12 +54,14 @@ for i=1:100
     BtrimAVG(i) = (B1primTrim(i)+B2primTrim(i))/2;
 end 
 
-figure('Name', 'BtrapMIN')
+% Wyniki
+figure('Name', 'Btrap min')
 plot(BtrapMIN)
-figure('Name', 'BtrimMIN')
-plot(BtrimMIN)
-figure('Name', 'BtrapAVG')
+figure('Name', 'Btrap avg')
 plot(BtrapAVG)
-figure('Name', 'BtrimAVG')
+
+figure('Name', 'Btrim min')
+plot(BtrimMIN)
+figure('Name', 'Btrim avg')
 plot(BtrimAVG)
 
